0e57d56ce313ca9f70dd0c7cbcf8b000f18cc758
Naresh Damera <142446681+nareshdamera@users.noreply.github.com>
2026-06-30 11:20:30 +0530

    Add addProduct method to IProduct interface

--- backend/src/main/java/com/restapidemo/restdemo/Models/DL/Services/IProduct.java
+++ backend/src/main/java/com/restapidemo/restdemo/Models/DL/Services/IProduct.java
@@ -7,4 +7,5 @@ import com.restapidemo.restdemo.Models.POJO.Product;
 public interface IProduct {
 	List<Product>allByCategory(String category);
 	List<Product>getAll();
+	String addProduct(Product product);
 }
