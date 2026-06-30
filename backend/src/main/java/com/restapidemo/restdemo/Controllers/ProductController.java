324facec404d17b3449438c13bd8320e54c11c8b
Naresh Damera <142446681+nareshdamera@users.noreply.github.com>
2026-06-30 11:18:48 +0530

    Add addProduct method to ProductController

--- backend/src/main/java/com/restapidemo/restdemo/Controllers/ProductController.java
+++ backend/src/main/java/com/restapidemo/restdemo/Controllers/ProductController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,9 @@ public class ProductController {
 	@GetMapping("/getall")
 	public List<Product> getall(){
 		return productService.getAll();
+	}
+	@PostMapping("/addproduct")
+	public String addProduct(@RequestBody Product product) {
+		return productService.addProduct(product);
 	}
 }
